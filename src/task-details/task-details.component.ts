3a945f10e5ced78bf7714813fcf4d67f81457649
DaniAkash <s.daniakash@gmail.com>
2017-03-17 18:10:26 +0530

    Task Details Page

--- src/task-details/task-details.component.ts
+++ src/task-details/task-details.component.ts
@@ -13,8 +13,14 @@ import { TaskService } from "../task.service";
   template: `
 <View [styleSheet]="styles.container">
   <Text [styleSheet]="styles.heading">
-    Details {{task.name}}
+    {{task.name}}
   </Text>
+  <View [styleSheet]="styles.backButton" opacityFeedback (tap)="goBack()">
+    <Text>Back</Text>
+  </View>
+  <View [styleSheet]="styles.details">
+    <Text>Priority - {{task.priority}}</Text>
+  </View>
 </View>
 `
 })
@@ -39,54 +45,24 @@ export class TaskDetails implements OnInit {
         top: 0,
         bottom: 0,
         justifyContent: 'flex-start',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
-      },
-      taskView: {
-        backgroundColor: 'whitesmoke',
-        marginBottom: 5,
-        padding: 15,
-        width: 300,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-      },
-      deleteButton: {
-        backgroundColor: 'red',
-        padding: 5,
-        borderRadius: 100,
       },
       heading: {
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
         marginTop: 40,
+        alignSelf: 'center',
       },
-      inputView: {
-        width: 300, 
-        height: 45,
-      },
-      instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-      },
-      button: {
-        width: 100,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        backgroundColor: '#32BAF5',
+      backButton: {
+        alignSelf: 'flex-start',
+        backgroundColor: 'blue',
+        marginLeft: 20,
         padding: 10,
-        margin: 20,
-        color: 'white'
       },
-      image: {
-        height: 100,
-        width: 100,
-        overflow: 'hidden',
-        position: 'absolute',
-        bottom: 0
-      }
+      details: {
+        alignSelf: 'center',
+      },
     });
   }
 
@@ -94,6 +70,10 @@ export class TaskDetails implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.task = this.taskService.getTask(+params['id']);
     });
+  }
+
+  goBack(): void {
+    this.locationStrategy.back();
   }
 
 }
